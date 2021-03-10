0ff415da2e9c2f81195b8d0a23a234ba23740ce7
Adam Exley <34522460+AdamExley@users.noreply.github.com>
2021-03-09 22:09:28 -0500

    Added stats for outliers removed

--- predict_3D.py
+++ predict_3D.py
@@ -1,3 +1,12 @@
+# Software License Agreement (Apache 2.0 License)
+#
+# Copyright (c) 2021, The Ohio State University
+# Center for Design and Manufacturing Excellence (CDME)
+# The Artificially Intelligent Manufacturing Systems Lab (AIMS)
+# All rights reserved.
+#
+# Author: Adam Exley
+
 from deepposekit.models import load_model
 from deepposekit.io import VideoReader
 import cv2
@@ -8,6 +17,7 @@ from robotpose.utils import *
 import pickle
 from robotpose import paths as p
 from robotpose.dataset import Dataset
+from robotpose.utils import reject_outliers_iqr
 
 limitMemory()
 
@@ -25,13 +35,18 @@ B_angles = ds.ang[:,4]
 model = load_model(r'C:\Users\exley\OneDrive\Documents\GitHub\DeepPoseRobot\models\set6_slu__B__CutMobilenet.h5')
 reader = VideoReader(ds.vid_path)
 predictions = model.predict(reader)
+
+# np.save('set6_output.npy',predToXYZ(predictions, ds.ply))
+# print("Predictions saved")
+
 pred_dict = predToDictList_new(predictions)
-pred_dict_xyz = predToXYZ(pred_dict, ds.ply)
+pred_dict_xyz = predToXYZdict(pred_dict, ds.ply)
 
 # Load video capture and make output
 cap = ds.vid
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter(p.video.replace(".avi","_overlay.avi"),fourcc, 12.5, (ds.resolution[1]*2,ds.resolution[0]))
+#out = cv2.VideoWriter(p.video.replace(".avi","_overlay.avi"),fourcc, 12.5, (ds.resolution[1]*2,ds.resolution[0]))
+out = cv2.VideoWriter(p.video.replace(".avi","_overlay.avi"),fourcc, 12.5, (ds.resolution[1],ds.resolution[0]))
 
 # Init predicted angle lists
 S_pred = []
@@ -50,7 +65,7 @@ while ret:
     coord_dict = pred_dict_xyz[i]
     
     # Predict S using the B joint position in reference to the R and U joints
-    S_pred_ang_BR = XYangle(coord_dict['B'][0]-coord_dict['R'][0], coord_dict['B'][2]-coord_dict['R'][2],(1,6))
+    S_pred_ang_BR = XYangle(coord_dict['B'][0]-coord_dict['R'][0], coord_dict['B'][2]-coord_dict['R'][2],(-1,5))
     S_pred_ang_LU = XYangle(coord_dict['B'][0]-coord_dict['U'][0], coord_dict['B'][2]-coord_dict['U'][2],(-1,5))
 
     # Take average of both angles (one tends to overshoot, one tends to undershoot)
@@ -75,7 +90,7 @@ while ret:
     #B_pred.append(B_pred_ang)
 
     # Put depth info on overlay
-    vizDepth_new(ds.ply[i], over)
+    #vizDepth_new(ds.ply[i], over)
     #Visualize lines
     viz(image, over, predictions[i])
 
@@ -83,7 +98,8 @@ while ret:
     dual[:,0:frame_width] = image
     dual[:,frame_width:frame_width*2] = over
 
-    out.write(dual)
+    #out.write(dual)
+    out.write(over)
     cv2.imshow("test",dual)
     cv2.waitKey(1)
     i+=1
@@ -151,9 +167,30 @@ L_err_std = np.std(np.abs(L_err))
 U_err_std = np.std(np.abs(U_err))
 #B_err_std = np.std(np.abs(B_err))
 
-print("Average error in degrees (after an offset is applied):")
-print(f"\tS: {avg_S_err}\n\tL: {avg_L_err}\n\tU: {avg_U_err}")
-print("Stdev of error in degrees (after an offset is applied):")
-print(f"\tS: {S_err_std}\n\tL: {L_err_std}\n\tU: {U_err_std}")
+print("\nAvg Error (deg):")
+print(f"\tS: {avg_S_err:.2f}\n\tL: {avg_L_err:.2f}\n\tU: {avg_U_err:.2f}")
+print("Stdev (deg):")
+print(f"\tS: {S_err_std:.2f}\n\tL: {L_err_std:.2f}\n\tU: {U_err_std:.2f}")
+
+
+# Determine average errors without outliers
+avg_S_err = np.mean(reject_outliers_iqr(np.abs(S_err)))
+avg_L_err = np.mean(reject_outliers_iqr(np.abs(L_err)))
+avg_U_err = np.mean(reject_outliers_iqr(np.abs(U_err)))
+#avg_B_err = np.mean(np.abs(B_err))
+S_err_std = np.std(reject_outliers_iqr(np.abs(S_err)))
+L_err_std = np.std(reject_outliers_iqr(np.abs(L_err)))
+U_err_std = np.std(reject_outliers_iqr(np.abs(U_err)))
+#B_err_std = np.std(np.abs(B_err))
+
+print("\nOutliers Removed:")
+print("Avg Error (deg):")
+print(f"\tS: {avg_S_err:.2f}\n\tL: {avg_L_err:.2f}\n\tU: {avg_U_err:.2f}")
+print("Stdev (deg):")
+print(f"\tS: {S_err_std:.2f}\n\tL: {L_err_std:.2f}\n\tU: {U_err_std:.2f}")
+
+
+
+
 
 plt.show()
