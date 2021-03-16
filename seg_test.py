d19e7ee05f899bc5bcdd8b7a2b7761892424e83f
Adam Exley <34522460+AdamExley@users.noreply.github.com>
2021-03-16 16:44:34 -0400

    Fixed for Arthur

--- seg_test.py
+++ seg_test.py
@@ -3,7 +3,7 @@ import os
 from robotpose.segmentation import RobotSegmenter
 a = RobotSegmenter(intrinsics='1280_720_color')
 for i in range(1,10):
-    img, b = a.segmentImage(fr'C:\Users\exley\OneDrive\Documents\GitHub\DeepPoseRobot\data\raw\extracted\set6_slu\202103020008{i}_og.png',
-        fr'C:\Users\exley\OneDrive\Documents\GitHub\DeepPoseRobot\data\raw\extracted\set6_slu\202103020008{i}_full.ply',
+    img, b = a.segmentImage(fr'data\raw\extracted\set6_slu\202103020008{i}_og.png',
+        fr'data\raw\extracted\set6_slu\202103020008{i}_full.ply',
         debug=True)
     cv2.imwrite(os.path.join("seg_test",f"{i}.png"),img)
