99f271285e7b296aa6f5e1dc8aef31c6a7b0de67
Adam Exley <34522460+AdamExley@users.noreply.github.com>
2021-03-22 19:55:16 -0400

    Fixed incorrect path reference

--- test_render.py
+++ test_render.py
@@ -1,39 +1,27 @@
-
+import cv2
 from robotpose.autoAnnotate import AutomaticKeypointAnnotator
-from robotpose.render import Aligner
+from robotpose.render import Aligner, Renderer
 #test_render()
 #test_render_with_class()
 
 
 
-# def test_render():
-
-#     objs = ['MH5_BASE', 'MH5_S_AXIS','MH5_L_AXIS','MH5_U_AXIS','MH5_R_AXIS_NEW','MH5_BT_UNIFIED_AXIS']
-#     names = ['BASE','S','L','U','R','BT']
-
-#     r = Renderer(objs, name_list=names)
-#     r.setMode('key')
-#     color_dict = r.getColorDict()
-#     anno = KeypointAnnotator(color_dict,'set6','B')
-    
-
-#     for frame in range(100):
-            
-#         r.setPosesFromDS(frame)
-#         color,depth = r.render()
-#         anno.annotate(color,frame)
-#         cv2.imshow("Render", color)
-#         cv2.waitKey(100)
-
-
-def test_align():
+def test_render():
 
     objs = ['MH5_BASE', 'MH5_S_AXIS','MH5_L_AXIS','MH5_U_AXIS','MH5_R_AXIS_NEW','MH5_BT_UNIFIED_AXIS']
     names = ['BASE','S','L','U','R','BT']
 
-    align = Aligner(objs, names, 'set6','B')
-    align.run()
+    r = Renderer(objs, 'set7', 'B',name_list=names)
+    r.setMode('seg')
+
+    
+
+    for frame in range(100):
+            
+        r.setPosesFromDS(frame)
+        color,depth = r.render()
+        cv2.imshow("Render", color)
+        cv2.waitKey(50)
 
 
-
-#test_align()
+test_render()
