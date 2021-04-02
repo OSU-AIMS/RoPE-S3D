e69e78008606e50ea04b23446e6137904c062426
Adam Exley <34522460+AdamExley@users.noreply.github.com>
2021-04-01 20:37:07 -0400

    Clean up automatic annotation script

--- annotate_auto.py
+++ annotate_auto.py
@@ -1,6 +1,21 @@
+# Software License Agreement (Apache 2.0 License)
+#
+# Copyright (c) 2021, The Ohio State University
+# Center for Design and Manufacturing Excellence (CDME)
+# The Artificially Intelligent Manufacturing Systems Lab (AIMS)
+# All rights reserved.
+#
+# Author: Adam Exley
+
 import argparse
-from robotpose.render import Renderer
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'  # or any {'0', '1', '2'}
+import tensorflow as tf
+
 from robotpose.autoAnnotate import AutomaticKeypointAnnotator, AutomaticSegmentationAnnotator
+from robotpose.render import Renderer
+
 
 def label(dataset, skeleton, preview):
 
@@ -16,7 +31,6 @@ def label(dataset, skeleton, preview):
 
     seg = AutomaticSegmentationAnnotator(objs, names, dataset, skeleton, renderer = rend, preview = preview)
     seg.run()
-
 
 
 if __name__ == "__main__":
