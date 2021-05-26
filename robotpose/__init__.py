4c119bef974d2d8c10fe1120262d01a8b45e3952
Adam Exley <34522460+AdamExley@users.noreply.github.com>
2021-05-25 21:53:43 -0400

    Added environment variable to fix issue in pixellib training

--- robotpose/__init__.py
+++ robotpose/__init__.py
@@ -1,4 +1,12 @@
 from .wizards import DatasetWizard
 from .data import Dataset, DatasetInfo, AutomaticAnnotator
 from .simulation import DatasetRenderer, LookupCreator
-from .prediction import Predictor
+
+
+"""
+When training a model for Pixellib, Prediction cannot be imported, as it enables eager execution in Tensorflow
+This causes errors to occur in training whenever the default model is loaded.
+"""
+import os 
+if "ROBOTPOSE_TRAINING" not in os.environ:
+    from .prediction import Predictor
--- train.py
+++ train.py
@@ -13,14 +13,10 @@ warnings.filterwarnings("ignore", category=FutureWarning)
 
 import argparse
 import os
-import random
-import requests
-import shutil
-
-#import pixellib # If you don't include this, it won't work. No idea why.
 
 from pixellib.custom_train import instance_custom_training
 
+os.environ["ROBOTPOSE_TRAINING"] = ""
 from robotpose import Dataset, DatasetRenderer
 from robotpose.training import ModelManager
 from robotpose.paths import Paths as p
@@ -29,39 +25,12 @@ from robotpose.paths import Paths as p
 def train(dataset, mode, batch):
     ds = Dataset(dataset)
 
-    # print("Splitting up data...")
-    # # Split set into validation and train
-    # for folder in ['train', 'test']:
-    #     path = os.path.join(ds.body_anno_path, folder)
-    #     if os.path.isdir(path):
-    #         shutil.rmtree(path)
-    #     os.mkdir(path)
-
-    # jsons = [x for x in os.listdir(ds.body_anno_path) if x.endswith('.json') and 'test' not in x and 'train' not in x]
-    # random.shuffle(jsons)
-
-    # valid_size = int(len(jsons) * valid)
-    # valid_list = jsons[:valid_size]
-    # train_list = jsons[valid_size:]
-
-    # for lst, folder in zip([valid_list, train_list],['test','train']):
-    #     path = os.path.join(ds.body_anno_path, folder)
-    #     for file in lst:
-    #         shutil.copy2(os.path.join(ds.body_anno_path, file), os.path.join(path, file))
-    #         shutil.copy2(os.path.join(ds.body_anno_path, file.replace('.json','.png')), os.path.join(path, file.replace('.json','.png')))
-
-    # print("Data Split.")
-    # default_model_path = r'models/segmentation/mask_rcnn_coco.h5'
-
-
-
     # Get names of classes for modeldata
     class_names = [x for x in DatasetRenderer(dataset, mode = {'body':'seg_full','link':'seg'}.get(mode)).color_dict]
 
     # Create a new model location
     mm = ModelManager()
     dest = mm.allocateNew(mode, dataset, class_names)
-    print(type(len(class_names)))
 
     # Configure training
     train_maskrcnn = instance_custom_training()
