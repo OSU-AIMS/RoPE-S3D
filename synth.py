b5e070b4f1b6918eb87f40e2540415065c0ae1b4
Adam Exley <34522460+AdamExley@users.noreply.github.com>
2021-09-03 10:53:58 -0400

    Cleanup; add synthetic prediction documentation

--- synth.py
+++ synth.py
@@ -1,10 +1,19 @@
+# Software License Agreement (Apache 2.0 License)
+#
+# Copyright (c) 2021, The Ohio State University
+# Center for Design and Manufacturing Excellence (CDME)
+# The Artificially Intelligent Manufacturing Systems Lab (AIMS)
+# All rights reserved.
+#
+# Author: Adam Exley
+
 from robotpose import Dataset, SyntheticPredictor
-from tqdm import tqdm
 
 angs = 'SLU'
 dataset = 'set63'
+number_of_poses = 10000
 
 
 ds = Dataset(dataset)
 synth = SyntheticPredictor(ds.camera_pose[0],'1280_720_color',8,angs)
-synth.run_batch(2500)
+synth.run_batch(number_of_poses)
